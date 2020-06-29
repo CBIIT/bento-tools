2cf1c7af574bbebf66b8c8728636119ff0a11b10
Ajay Doddapaneni <doddapaneni.ajay@gmail.com>
2020-06-29 15:00:47 -0400

    adding readme

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,17 +1,13 @@
 /* eslint-disbale */
 import React from 'react';
-import { IconButton, Typography } from '@material-ui/core';
-import AddCircleIcon from '@material-ui/icons/AddCircle';
-import DeleteIcon from '@material-ui/icons/Delete';
-import EditIcon from '@material-ui/icons/Edit';
-import SwapHorizIcon from '@material-ui/icons/SwapHoriz';
-import VisibilityIcon from '@material-ui/icons/Visibility';
-import { action } from '@storybook/addon-actions';
+
 import { storiesOf } from '@storybook/react';
 import MUIDatatable from '../components/datatables/MUIDataTable';
 import users from '../stubs/users.json';
-import usersWithCars from '../stubs/usersWithCars.json';
+import footerData from '../stubs/footer.json';
 import Header from '../components/headers';
+import Footer from '../components/footer';
+
 import nihLogo from './icdc_nih_logo.svg';
 import easter2000 from './Canine2000.png';
 
@@ -44,5 +40,6 @@ storiesOf('Header', module)
   .add('Easter Header with custom easter', () => <Header easter={easter2000} />)
   .add('Easter Header with custom logo and eas', () => <Header logo={nihLogo} easter={easter2000} />);
 
+storiesOf('Footer', module).add('Footer', () => <Footer data={footerData} />);
 storiesOf('MUIDatatable', module).add('left Selectable', () => <MUIDatatable columns={columns} data={data} title={title} selectCellPostion="right" />);
 storiesOf('MUIDatatable', module).add('right Selectable', () => <MUIDatatable columns={columns} data={data} options={options} title={title} selectCellPostion="right" />);
