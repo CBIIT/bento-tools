ae54a28448c86ee0c439aa5883154592c46221aa
skiran86 <skphp9@gmail.com>
2020-11-02 16:05:21 -0500

    Removing project specific files

--- src/utils/env.js
+++
@@ -1,9 +0,0 @@
-const processEnv = typeof process !== 'undefined' ? process.env : {};
-const injectedEnv = window && window.injectedEnv ? window.injectedEnv : {};
-
-const env = {
-  ...processEnv,
-  ...injectedEnv,
-};
-
-export default env;
