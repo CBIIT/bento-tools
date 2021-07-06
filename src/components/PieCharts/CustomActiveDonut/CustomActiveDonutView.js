4b9207460c1c2dc2a95926ad8e05d4171cdc5d5c
Ajay Doddapaneni <doddapaneni.ajay@gmail.com>
2021-07-06 09:50:53 -0400

    lint fix

--- src/components/PieCharts/CustomActiveDonut/CustomActiveDonutView.js
+++ src/components/PieCharts/CustomActiveDonut/CustomActiveDonutView.js
@@ -91,7 +91,7 @@ class CustomActiveDonut extends PureComponent {
   constructor(props) {
     super(props);
     const { data } = props;
-    const index = data.length !== undefined? data.length - 1 : 0;
+    const index = data.length !== undefined ? data.length - 1 : 0;
     this.state = { activeIndex: index };
   }
 
