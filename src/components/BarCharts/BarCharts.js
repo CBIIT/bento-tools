8a5e0981398359ccf3d9e911f199ba35be64cb1f
Udosen <udosent2@nci-02278094-ml.nih.gov>
2023-05-31 10:58:54 -0500

    barChart fix

--- src/components/BarCharts/BarCharts.js
+++ src/components/BarCharts/BarCharts.js
@@ -1,4 +1,4 @@
-import React , { PureComponent } from 'react';
+import React, { PureComponent } from 'react';
 import Chart, {
   Series,
   CommonSeriesSettings,
@@ -14,13 +14,12 @@ import Chart, {
   Font,
   Title,
   Tick,
-  HoverStyle
+  HoverStyle,
 } from 'devextreme-react/chart';
 
 const enable = true;
 
 class BarChart extends PureComponent {
-
   constructor(props) {
     super(props);
     this.state = this.props;
@@ -28,7 +27,7 @@ class BarChart extends PureComponent {
 
   /** repeat palette to argument length */
   extendPalette = (palette, data) => {
-    if (palette && palette.length > 0){
+    if (palette && palette.length > 0) {
       return [].concat(...new Array(Math
         .ceil(data.length / palette.length)).fill(palette));
     }
@@ -41,13 +40,16 @@ class BarChart extends PureComponent {
       const value = props[key];
       properties[key] = { ...defaultVal, ...value };
     }
-    this.state = { ...this.state, ...properties};
+    // update data
+    const { data } = this.props;
+    this.state = { ...this.state, ...properties, data };
   }
 
   render() {
     this.setConfigValues(this.props);
-    const { data, palette, mode, tooltipContent, size,
-      tooltipConfig, seriesSetting, argument, value, type 
+    const {
+      data, palette, mode, tooltipContent, size,
+      tooltipConfig, seriesSetting, argument, value, type,
     } = this.state;
 
     return (
@@ -58,80 +60,79 @@ class BarChart extends PureComponent {
           paletteExtensionMode={mode}
         >
 
-        /** chart dimension */
-        <Size height={size.height} width={size.width} />
+          /** chart dimension */
+          <Size height={size.height} width={size.width} />
 
-        //** indiviual series setting */
-        <Series type={type}></Series>
+          //** indiviual series setting */
+          <Series type={type} />
 
-        //** common series setting */
-        <CommonSeriesSettings
-          argumentField={argument.field}
-          valueField={value.field}
-          type={type}
-          ignoreEmptyPoints={seriesSetting.ignoreEmptyPoints}
-          showInLegend={seriesSetting.showInLegend}
-          barWidth={seriesSetting.maxBarWidth}
-          hoverMode={seriesSetting.hoverMode}
-          border={seriesSetting.border}
-          color={seriesSetting.color}
-          hoverStyle={seriesSetting.hoverStyle}
-          ignoreEmptyPoints={seriesSetting.ignoreEmptyPoints}
-        >
-          <HoverStyle
-             border={seriesSetting.hoverStyle.border}
-             color={seriesSetting.hoverStyle.color}
-             dashStyle={seriesSetting.hoverStyle.dashStyle}
-             width={seriesSetting.hoverStyle.width}
-          />
-          <Label
-            visible={seriesSetting.label.visible}
-            backgroundColor={seriesSetting.label.backgroundColor}
-            alignment={seriesSetting.label.alignment}
+          //** common series setting */
+          <CommonSeriesSettings
+            argumentField={argument.field}
+            valueField={value.field}
+            type={type}
+            ignoreEmptyPoints={seriesSetting.ignoreEmptyPoints}
+            showInLegend={seriesSetting.showInLegend}
+            barWidth={seriesSetting.maxBarWidth}
+            hoverMode={seriesSetting.hoverMode}
+            border={seriesSetting.border}
+            color={seriesSetting.color}
+            hoverStyle={seriesSetting.hoverStyle}
+            ignoreEmptyPoints={seriesSetting.ignoreEmptyPoints}
           >
-            <Font {...seriesSetting.label.size} />
-          </Label>
-          <Border 
-            color={seriesSetting.border.color}
-            dashStyle={seriesSetting.border.dashStyle}
-            visible={seriesSetting.border.visible}
-            width={seriesSetting.border.width}
-          />
-        </CommonSeriesSettings>
+            <HoverStyle
+              border={seriesSetting.hoverStyle.border}
+              color={seriesSetting.hoverStyle.color}
+              dashStyle={seriesSetting.hoverStyle.dashStyle}
+              width={seriesSetting.hoverStyle.width}
+            />
+            <Label
+              visible={seriesSetting.label.visible}
+              backgroundColor={seriesSetting.label.backgroundColor}
+              alignment={seriesSetting.label.alignment}
+            >
+              <Font {...seriesSetting.label.size} />
+            </Label>
+            <Border
+              color={seriesSetting.border.color}
+              dashStyle={seriesSetting.border.dashStyle}
+              visible={seriesSetting.border.visible}
+              width={seriesSetting.border.width}
+            />
+          </CommonSeriesSettings>
 
-        <SeriesTemplate nameField={argument.field} />
-        
-        /** value or y axis */
-        <ValueAxis allowDecimals={value.allowDecimals}>
-          <Tick {...value.tick}></Tick>
-          <Title text={value.title.text}>
-            <Font {...value.title} />
-          </Title>
-          <ChartGrid visible={value.chartGrid.visible} />
-          <Label position={value.label.position} > 
-            <Font {...value.label} />
-          </Label>
-        </ValueAxis>
+          <SeriesTemplate nameField={argument.field} />
 
-        /** argument axis */
-        <ArgumentAxis>
-          <Title text={argument.title.text}>
-            <Font  {...argument.title} />
-          </Title>
-          <Label visible={argument.visible} position={argument.label.position}>
-            <Font {...argument.label} />
-          </Label>
-        </ArgumentAxis>
+          /** value or y axis */
+          <ValueAxis allowDecimals={value.allowDecimals}>
+            <Tick {...value.tick} />
+            <Title text={value.title.text}>
+              <Font {...value.title} />
+            </Title>
+            <ChartGrid visible={value.chartGrid.visible} />
+            <Label position={value.label.position}>
+              <Font {...value.label} />
+            </Label>
+          </ValueAxis>
 
-        /** tooltip */
-        {tooltipContent && (
-          <CartToolTip enabled={tooltipConfig.enable} contentRender={tooltipContent} >
+          /** argument axis */
+          <ArgumentAxis>
+            <Title text={argument.title.text}>
+              <Font {...argument.title} />
+            </Title>
+            <Label visible={argument.visible} position={argument.label.position}>
+              <Font {...argument.label} />
+            </Label>
+          </ArgumentAxis>
+
+          /** tooltip */
+          {tooltipContent && (
+          <CartToolTip enabled={tooltipConfig.enable} contentRender={tooltipContent}>
             <Font family={tooltipConfig.family} size={tooltipConfig.size} />
             <Border color={tooltipConfig.color} width={tooltipConfig.width} />
             <Shadow blur={tooltipConfig.blur} offsetY={tooltipConfig.offsetY} opacity={tooltipConfig.opacity} />
           </CartToolTip>
-          )
-        }
+          )}
         </Chart>
       </>
     );
@@ -142,7 +143,7 @@ BarChart.defaultProps = {
   type: 'bar',
   palette: '',
   defaultConfig: {
-    //** Xaxis config value */
+    //* * Xaxis config value */
     argument: {
       field: '',
       visible: false,
@@ -153,7 +154,7 @@ BarChart.defaultProps = {
         family: 'Open Sans',
         opacity: 1,
         size: 14,
-        weight: 400 
+        weight: 400,
       },
       size: 12,
       label: {
@@ -161,10 +162,10 @@ BarChart.defaultProps = {
         size: 12,
         position: 'inside',
         staggeringSpacing: 10,
-      }
+      },
     },
-  
-    //** Yaxis config value */ 
+
+    //* * Yaxis config value */
     value: {
       field: 'count',
       title: {
@@ -173,7 +174,7 @@ BarChart.defaultProps = {
         family: 'Open Sans',
         opacity: 1,
         size: 14,
-        weight: 400 
+        weight: 400,
       },
       size: 12,
       allowDecimals: false,
@@ -185,10 +186,10 @@ BarChart.defaultProps = {
         position: 'outside',
       },
       tick: {
-        visible: true
-      }
+        visible: true,
+      },
     },
-  
+
     /** common series setting */
     seriesSetting: {
       ignoreEmptyPoints: true,
@@ -221,8 +222,8 @@ BarChart.defaultProps = {
         },
         color: undefined,
         dashStyle: 'solid',
-        width: 3
-      }
+        width: 3,
+      },
     },
 
     /** chart dimension */
@@ -231,7 +232,7 @@ BarChart.defaultProps = {
       width: 300,
     },
 
-    /** tooltip configuration */ 
+    /** tooltip configuration */
     tooltipConfig: {
       enable: true,
       family: 'Open Sans',
@@ -242,8 +243,8 @@ BarChart.defaultProps = {
       offsetY: 0,
       opacity: 0,
       interactive: false,
-    }
-  }
+    },
+  },
 };
 
 export default BarChart;
